8b1f76d96012eb76f903a9c8754931faf495b11f
guangamber <102640041+guangamber@users.noreply.github.com>
2023-11-28 14:35:21 +0800

    Refactor for pip setup

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+def read_requirements():
+    with open('requirements.txt') as req:
+        return [line.strip() for line in req if line.strip() and not line.startswith('#')]
+
+setup(
+    name='otx2misp', 
+    version='1.0',
+    author='guangamber',
+    author_email='guangamber',
+    description='A short description of your project', 
+    long_description=open('README.md').read(), 
+    long_description_content_type='text/markdown',
+    url='https://github.com/guangamber/otx2misp', 
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    install_requires=read_requirements(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache License', 
+        'Programming Language :: Python :: 3', 
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    entry_points={
+        'console_scripts': [
+            'otx2misp=otx2misp.main:main'
+        ]
+    },
+)
---
+++ src/otx2misp/core.py
@@ -0,0 +1,114 @@
+# core.py
+import requests
+from pymisp import PyMISP, MISPEvent
+from OTXv2 import OTXv2
+from datetime import datetime
+from .utils import convert_string_to_datetime, check_if_empty_att
+
+try:
+    requests.packages.urllib3.disable_warnings()
+except:
+    pass
+
+def initialize_misp(config_data:dict) -> PyMISP:
+    try:
+        return PyMISP(config_data['misp'].get('url'), config_data['misp'].get('api_key'), ssl=False)
+    except Exception as ex:
+        raise Exception("Cannot connect to MISP instance. Check server url and api key.")
+
+def initialize_otx(config_data:dict) -> OTXv2:
+    try:
+        return OTXv2(api_key=config_data['otx'].get('api_key'),verify=False)
+    except Exception as e:
+        raise Exception(f"Cannot connect to OTX instance. Check api key. Details: {e}")
+    
+def get_pulses(logger, otx: OTXv2, from_timestamp=None):
+    if from_timestamp is None:
+        logger.debug("Retrieving all Pulses (no timestamp)")
+        pulses = otx.getall()
+    else:
+        logger.debug("Retrieving pulses since " + from_timestamp)
+        pulses = otx.getsince(from_timestamp)
+
+    return pulses
+
+def add_tags_to_event(logger, misp: PyMISP, event: MISPEvent, pulse: dict):
+    misp.tag(event, "Threat Source:OSINT")
+    if 'tlp' in pulse:
+        tag = f"tlp:{pulse['tlp']}"
+        misp.tag(event, tag)
+
+    if 'tags' in pulse:
+        for pulse_tag in pulse['tags'] and not pulse_tag.startswith('#'):
+            misp_tags = misp.search_tags(tagname=pulse_tag.lower())
+            if misp_tags:
+                misp.tag(misp_entity=event, tag=misp_tags[0]['Tag'])
+            else:
+                misp.tag(misp_entity=event, tag=pulse_tag)
+                logger.info("Tag does not exist. Added new tag:" + pulse_tag)
+
+def add_iocs_to_event(logger,event: MISPEvent, pulse: dict):
+    ioc_type_mapping = {
+        'IPv4': 'ip-src',
+        'IPv6': 'ip-src',
+        'domain': 'domain',
+        'YARA': 'yara',
+        'hostname': 'hostname',
+        'email': 'email',
+        'URL': 'url',
+        'MUTEX': 'mutex',
+        'CVE': 'other',
+        'FileHash-MD5': 'md5',
+        'FileHash-SHA1': 'sha1',
+        'FileHash-SHA256': 'sha256',
+        'FileHash-PEHASH': 'pehash',
+        'FileHash-IMPHASH': 'imphash'
+    }
+    for key, value in pulse.items():
+        if key in ['id','name','author_name','modified','tlp','created','revision','public','tags']:
+            continue
+        elif key == 'indicators':
+            for ioc in value:
+                ioc_type = ioc['type']
+                attr_type = ioc_type_mapping.get(ioc_type, None)
+                if attr_type:
+                    attr_value = ioc['indicator'] if ioc_type != 'YARA' else ioc['content']
+                    if ioc_type == 'CVE':
+                        attr_value = "CVE: " + attr_value
+                    event.add_attribute(attr_type, attr_value)
+        elif key in ['malware_families', 'targeted_countries', 'adversary','attack_ids']:
+            if not check_if_empty_att(value):
+                prefix = key.replace('_', ' ').capitalize() + ': '
+                event.add_attribute("other", prefix + str(value))
+        elif key == 'references':
+            if not check_if_empty_att(value):
+                for r in value:
+                    att_kwargs = {'category': 'External analysis'}
+                    event.add_attribute("link", r, **att_kwargs)
+        elif key == 'description':
+            if not check_if_empty_att(value):
+                att_kwargs = {'category': 'External analysis'}
+                event.add_attribute("comment", value, **att_kwargs)
+        else:
+            logger.warning("Unsupported IOC type:" + key)
+
+def create_event(logger, misp: PyMISP, pulse: dict):
+    new_event = MISPEvent()
+    new_event.distribution = 0
+    new_event.analysis = 2
+    new_event.info = pulse['name']
+    try:
+        dt = convert_string_to_datetime(pulse['created'])
+    except ValueError:
+        logger.error("Cannot parse Pulse 'created' date.")
+        dt = datetime.utcnow()
+    new_event.date = dt.strftime('%Y-%m-%d')
+    new_event.timestamp = dt
+    add_iocs_to_event(new_event,pulse)
+    result = misp.add_event(new_event)
+    add_tags_to_event(misp,result,pulse)
+    logger.debug("Pulse addedt MISP, ID:" +result['Event']['id'] + ", name: " + pulse['name'])
+
+
+def update_event(logger, misp: PyMISP, pulse: dict):
+    logger.debug("not yet implemented")
