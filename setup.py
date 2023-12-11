15f5aa9054fc5c9ee292e977ddc2d528fcbdafcd
guangamber <102640041+guangamber@users.noreply.github.com>
2023-12-11 13:16:00 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read_requirements():
 
 setup(
     name='otx2misp', 
-    version='1.1',
+    version='1.0',
     author='guangamber',
     author_email='guangamber',
     description='A short description of your project', 
