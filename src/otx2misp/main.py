ec43d3cf98c6c63e891cf14ff15fc8884772a3b6
guangamber <102640041+guangamber@users.noreply.github.com>
2023-12-11 13:56:17 +0800

    Update main.py

--- src/otx2misp/main.py
+++ src/otx2misp/main.py
@@ -1,18 +1,18 @@
 import argparse
 import logging
-from .utils import setup_logger,load_config, get_start_date
+from .utils import setup_global_logger,load_config, get_start_date
 from .core import initialize_misp,initialize_otx,get_pulses,create_event,update_event
 
 def main():
     parser = argparse.ArgumentParser(description="Provide the absolute path of config file. Refer to sample-misp.ini for sample.")
     parser.add_argument('file_path', help="The path to the misp.ini file.")
     args = parser.parse_args()
-    logger = logging.getLogger("otx2misp")
+    logger = logging.getLogger('otx2misp')
     config_data = {}
     try:
         config_data = load_config(args.file_path)
+        setup_global_logger(config_data)
         logger.debug("Config file loaded: " + args.file_path)
-        logger = setup_logger(logger, config_data)
     except Exception as e:
         logger.error(f"An error occurred: {e}")
     
@@ -23,7 +23,7 @@ def main():
     if otx.get_user("AlienVault"):
         logger.debug("OTX connection is verified.")
     
-    pulses = get_pulses(logger,otx, get_start_date(config_data['otx']['last_days']))
+    pulses = get_pulses(otx, get_start_date(config_data['otx']['last_days']))
     if len(pulses) == 0:
         logger.info("No new pulses retrieved. Ending.")
     else:
@@ -31,11 +31,11 @@ def main():
             pulse_name = pulse['name']
             result = misp.search_index(eventinfo=pulse_name)
             if len(result)==0:
-                create_event(logger, misp, pulse)
+                create_event(misp, pulse)
                 logger.info("Importing pulse done:" + pulse_name)
             else:
                 logger.info("Pulse already imported. Going to update this pulse:" + pulse_name)
-                update_event(logger, misp, pulse)
+                update_event(misp, pulse)
     logger.info("Done with importing.")
 if __name__ == "__main__":
     main()
